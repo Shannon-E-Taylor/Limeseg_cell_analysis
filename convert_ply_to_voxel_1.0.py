e4674fa4b2440eeaf469961e9d524ff39b5a948d
Shannon Taylor <shannon.elisa.taylor@gmail.com>
2023-11-01 14:34:28 +0000

    comment convert_ply_to_voxel_1.0.py

--- convert_ply_to_voxel_1.0.py
+++ convert_ply_to_voxel_1.0.py
@@ -5,28 +5,22 @@ import numpy as np
 import sys
 from skimage.io import imread 
 
-# f_in = sys.argv[0]
+# get the file paths from the command line 
+path_to_output_image = sys.argv[1] # path to save the new label image to 
+path_to_input_image = sys.argv[2] # path to the original image you segmented, to read image dimensions from  
+path_to_limeseg_folder = sys.argv[3] # path to the folder where you saved your limeseg data 
 
-f_out = sys.argv[1]
-path_to_image = sys.argv[2]
-f_in = sys.argv[3]
+# Limeseg automatically saves each cell in its own folder
+# So to read all the cells we want a list of all of these folders 
+cell_list = os.listdir(path_to_limeseg_folder)
 
-cell_list = os.listdir(f_in)
-
-img_zeros = imread(path_to_image).astype(np.int) * 0 
-
-# img_zeros = img_zeros
-
-print('inputs ok')
-
-img_zeros = img_zeros * 0 
-
-cell_list = os.listdir(f_in)
+# generate a template image of the same dimensions as the original image 
+img_zeros = imread(path_to_input_image).astype(np.int) * 0 
 
 for cell in cell_list: 
     path = f_in + '/' + cell + '/T_1.ply'
+    # only try to load the cell if we've successfully generated a label image in the previous script 
     if os.path.exists(path + '_.npy'): 
-        print(cell)
         cell_mask = np.load(path + '_.npy')
         x, y, z = np.nonzero(cell_mask)
         pointcloud = o3d.io.read_point_cloud(path)
@@ -37,9 +31,10 @@ for cell in cell_list:
             z_put.astype(int), y_put, x_put 
             ] = int(cell.split('_')[-1])
 
-print(np.max(img_zeros))
+
+print("Got up to cell number {np.max(img_zeros)}")
 
 np.savez_compressed(f_out, seg=img_zeros)
 
-
+print("Saved and finished!") 
 
